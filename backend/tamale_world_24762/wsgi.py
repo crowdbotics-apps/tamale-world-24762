bffd1a4481e9f97283e2e906953446881372ab10
Crowdbotics <team@crowdbotics.com>
2021-02-28 19:33:30 +0000

    Initial commit.

---
+++ backend/tamale_world_24762/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tamale_world_24762 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tamale_world_24762.settings')
+
+application = get_wsgi_application()
